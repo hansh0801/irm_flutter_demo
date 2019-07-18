788023c91b250b1a47bbbca5611b5af9149ec5bb
jsm <s1930wjd@naver.com>
2019-07-18 17:23:19 +0900

    updated: add functions(get patients' name list, search patients' name)

--- lib/get_patient_data.dart
+++ lib/get_patient_data.dart
@@ -2,6 +2,9 @@ import 'japiRequest.dart';
 import 'dart:convert';
 import 'irm_auth.dart';
 
+///인자로 받은 그룹의 환자 정보 리스트를 반환
+///input: group_id
+///output: groups' patient list
 Future<List> getPatientList(group_id) async {
   var queryParameters = {
     'vgroup_key_list': '{"$group_id"}',
@@ -11,7 +14,9 @@ Future<List> getPatientList(group_id) async {
   return jsonResult['patient_list'];
 }
 
-///이름만 들어있는 리스트 반환하는 함수
+///자신이 속한 그룹의 이름 리스트를 반환
+///input: nothing
+///output: my groups' list of name
 Future<List> getGroupNameList() async{
   var jsonData = await getGroupSearchBelonged();
   List<String> groupList = [];
@@ -20,4 +25,31 @@ Future<List> getGroupNameList() async{
   }
 
   return groupList;
+}
+
+///인자로 받은 그룹의 환자 이름 리스트를 반환
+///input: group_id
+///output: list of patients' name in group
+Future<List> getPatientNameList(group_id) async{
+  List patientGroup = await getPatientList(group_id);
+  List<String> patientName = [];
+  for(var name in patientGroup)
+    patientName.add(name['patient_name']);
+
+  return patientName;
+}
+
+///그룹 내 환자 이름 검색
+///input: group_id, patient_name
+///output: list of patients'name
+Future<List> searchPatientName(group_id, patient_name) async {
+  List<String> result = [];
+  List<String> patientList = await getPatientNameList(group_id);
+
+  for(String name in patientList){
+    if(name.contains(patient_name))
+      result.add(name);
+  }
+
+  return result;
 }
